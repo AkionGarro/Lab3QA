8312eecca9ae4d6bf2b4c6ac335640d35a50cdd3
AkionGarro <carloscamp1008@estudianTEC.cr>
2023-05-30 14:37:41 -0600

    Test1

--- App/cypress/e2e/test.cy.ts
+++ App/cypress/e2e/test.cy.ts
@@ -1,21 +1,32 @@
+import { waitForDebugger } from 'inspector';
+
 require('cypress-xpath');
-describe('Login', function () {
+describe('Documentos Acueducto', function () {
   beforeEach(function () {
-    cy.visit('https://tecdigital.tec.ac.cr/');
+    cy.visit('https://www.munisc.go.cr/Paginas/Visitantes/Documentos.aspx');
   });
 
-  it('Nombre de usuario requerido', function () {
+  it('Formulario de disponibilidad de agua', function () {
+    //cambiar a buscar por departamento
     cy.xpath(
-      '/html/body/div[1]/div/div[2]/div[1]/div[2]/div[3]/div[3]/form/div[4]/div/button'
+      '/html/body/form/div[3]/div[2]/div[1]/div[3]/div/div[2]/fieldset/div[1]/label[2]/input'
     )
       .click()
-      .then(function () {
+      .then(() => {
         cy.xpath(
-          '/html/body/div[1]/div/div[2]/div[1]/div[2]/div[3]/div[3]/form/div[1]/div/md-input-container/div[2]/div'
-        ).should('have.text', 'Por favor ingrese un correo válido.');
+          '/html/body/form/div[3]/div[2]/div[1]/div[3]/div/div[2]/fieldset/div[2]/select'
+        )
+          .select('Acueducto')
+          .then(() => {
+            cy.xpath(
+              '/html/body/form/div[3]/div[2]/div[1]/div[3]/div/div[2]/fieldset/div[4]/div/table/tbody/tr[7]/td[1]'
+            ).should(
+              'have.text',
+              'Formulario solicitud para diponibilidad de agua'
+            );
+          });
       });
   });
-
   afterEach(function () {
     cy.reload();
   });
