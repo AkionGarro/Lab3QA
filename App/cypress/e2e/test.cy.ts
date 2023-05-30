3040f735822097e570d1a824b5ccc8544fba621f
AkionGarro <carloscamp1008@estudianTEC.cr>
2023-05-30 14:37:55 -0600

    Test2

--- App/cypress/e2e/test.cy.ts
+++ App/cypress/e2e/test.cy.ts
@@ -31,3 +31,31 @@ describe('Documentos Acueducto', function () {
     cy.reload();
   });
 });
+
+describe('Documentos Administración', function () {
+  beforeEach(function () {
+    cy.visit('https://www.munisc.go.cr/Paginas/Visitantes/Documentos.aspx');
+  });
+
+  it('Formulario de administración', function () {
+    //cambiar a buscar por departamento
+    cy.xpath(
+      '/html/body/form/div[3]/div[2]/div[1]/div[3]/div/div[2]/fieldset/div[1]/label[2]/input'
+    )
+      .click()
+      .then(() => {
+        cy.xpath(
+          '/html/body/form/div[3]/div[2]/div[1]/div[3]/div/div[2]/fieldset/div[2]/select'
+        )
+          .select('Administración')
+          .then(() => {
+            cy.xpath(
+              '/html/body/form/div[3]/div[2]/div[1]/div[3]/div/div[2]/fieldset/div[4]/div/table/tbody/tr/td/div/span'
+            ).should('have.text', 'No existen documentos relacionados.');
+          });
+      });
+  });
+  afterEach(function () {
+    cy.reload();
+  });
+});
